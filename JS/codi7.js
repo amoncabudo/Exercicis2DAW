a1a1039cbb207f562ea3fc6568bb0bcf14b712ca
amoncabudo <amoncayobudo@cendrassos.net>
2024-09-17 16:48:37 +0200

    Exercicis php i js

---
+++ JS/codi7.js
@@ -0,0 +1,12 @@
+var numero = prompt("Escriu un numero");
+
+function primer(numero) {
+  for (i = 2; i < numero; i++) {
+    if (numero % i == 0) {
+      return numero + " no es primer";
+    }
+  }
+  return numero + " es primer";
+}
+
+console.log(primer(numero));
---
+++ MP06/Act0/codi.js
@@ -0,0 +1,2 @@
+alert("Alex Moncayo Budó MP06"); //Missatge alerta amb el meu nom 
+alert("Sóc el segond script"); //Missatge alerta segon script
