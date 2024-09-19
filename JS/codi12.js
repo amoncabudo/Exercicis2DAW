4446fc9fd0b62fe0df860860cebb47f27c5e8d5e
amoncabudo <amoncayobudo@cendrassos.net>
2024-09-19 16:35:16 +0200

    modificacio act12 js

--- JS/codi12.js
+++ JS/codi12.js
@@ -3,9 +3,9 @@ var paraula = prompt("Quina paraula vols comprovar")
 function palindromo(text) {
     const textinvert = text.split("").reverse().join("");
     if (text === textinvert) {
-      console.log("es palindromo");
+      document.write ("es palindromo");
     } else {
-      console.log("no es palindromo");
+      document.write ("no es palindromo");
     };
   
   };
