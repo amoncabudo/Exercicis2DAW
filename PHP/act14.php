921e0ec7133e5d375ab193cacb36d04df118c869
amoncabudo <amoncayobudo@cendrassos.net>
2024-09-20 16:59:34 +0200

    Modificacio act12 php, i act14 php

--- PHP/act14.php
+++ PHP/act14.php
@@ -6,24 +6,24 @@
         <button button type="submit">Enviar</button>
     </form>
 <?php
-    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['text'])) {
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['paraula'])) {
         $text = strtolower($_POST['paraula']); 
         $lletres = [];
 
-    for ($i = 0; $i < $strlen($text); $i++) {
-    $lletra = $text[$i]; 
-
-    if (preg_match('/[a-z]/', $lletra)) {
-        if (isset($lletres[$lletra])) {
-            $lletres[$lletra]++; 
-        } else {
-            $lletres[$lletra] = 1; 
+        for ($i = 0; $i < strlen($text); $i++) {
+            $lletra = strtolower($text[$i]);
+            
+            if (preg_match('/[a-z]/', $lletra)) {
+                if (isset($lletres[$lletra])) {
+                    $lletres[$lletra]++;
+                } else {
+                    $lletres[$lletra] = 1;
+                }
+            }
         }
-    }
-}
-
-foreach ($lletres as $lletra => $count) {
-    echo $lletra . ': ' . $count . '<br>';  
-}
+        
+        foreach ($lletres as $lletra => $count) {
+            echo "$lletra : $count <br>";
+        }
     }
         ?>
